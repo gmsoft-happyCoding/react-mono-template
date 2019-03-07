0cc58f9cd2a00c29b0cbff5212e04b7e4340fc36
angular-moon <xiaoxuss@163.com>
2019-03-07 18:01:37 +0800

    fix eslint import/resolver error

--- packages/app/config/env.js
+++ packages/app/config/env.js
@@ -7,10 +7,7 @@ const paths = require('./paths');
 // Make sure that including paths.js after env.js will read .env variables.
 delete require.cache[require.resolve('./paths')];
 
-const NODE_ENV = process.env.NODE_ENV;
-if (!NODE_ENV) {
-  throw new Error('The NODE_ENV environment variable is required but was not specified.');
-}
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 // 部署环境
 const DEPLOY_ENV = process.env.DEPLOY_ENV;
