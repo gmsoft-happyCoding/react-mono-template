b9bc39a430368817578f668309293f10244a6ff4
angular-moon <xiaoxuss@163.com>
2019-04-28 14:57:19 +0800

    docWrapper remove use LocaleProvider

--- packages/components/src/DocWrapper.tsx
+++ packages/components/src/DocWrapper.tsx
@@ -1,17 +1,13 @@
 import React, { ReactElement } from 'react';
 import { Provider } from 'react-redux';
-import { LocaleProvider } from 'antd';
 import { utils } from 'common';
-import zhCN from 'antd/es/locale-provider/zh_CN';
 
 type Props = {
   children: ReactElement<any>;
 };
 
 const Wrapper = ({ children }: Props) => (
-  <LocaleProvider locale={zhCN}>
-    <Provider store={utils.stateContainer._store}>{children}</Provider>
-  </LocaleProvider>
+  <Provider store={utils.stateContainer._store}>{children}</Provider>
 );
 
 export default Wrapper;
