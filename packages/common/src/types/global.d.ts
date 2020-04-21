844f89c65fd4d0b9ba7e2ac2a44ac5b39d387e99
angular-moon <xiaoxuss@163.com>
2020-04-21 20:16:38 +0800

    add types DvaLoadingState

--- packages/common/src/types/global.d.ts
+++ packages/common/src/types/global.d.ts
@@ -59,3 +59,9 @@ interface Window {
 }
 
 type Omit = Pick<T, Exclude<keyof T, K>>;
+
+interface DvaLoadingState {
+  global: boolean;
+  models: { [type: string]: boolean | undefined };
+  effects: { [type: string]: boolean | undefined };
+}
