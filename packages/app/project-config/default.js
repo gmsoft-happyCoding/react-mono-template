46dedd92903f0dc30c6041f225cc9c6c56eb658b
angular-moon <xiaoxuss@163.com>
2020-07-23 17:24:48 +0800

    update lock file

--- packages/app/project-config/default.js
+++ packages/app/project-config/default.js
@@ -4,9 +4,9 @@ module.exports = {
     // 网关服务器base路径
     REACT_APP_API_GATEWAY_BASE: 'http://easy-mock.gm/mock/5c514bf9cd2f550e9dfbb515/',
     // CDN服务器地址
-    REACT_APP_CDN_SERVER: '//cdn.gmsoftdev.com',
+    REACT_APP_CDN_SERVER: '//cdn.gm',
     // 组件注册服务器
-    REACT_APP_COMPONENT_REGISTRY_SERVER: '//registry.gmsoftdev.com',
+    REACT_APP_COMPONENT_REGISTRY_SERVER: '//registry.gmsofttest.com',
   },
   // plugins: [require('./plugins/plugin-demo')],
 };
--- packages/components/project-config/default.js
+++ packages/components/project-config/default.js
@@ -4,7 +4,7 @@ module.exports = {
     // 网关服务器base路径
     REACT_APP_API_GATEWAY_BASE: 'http://easy-mock.gm/mock/5c514bf9cd2f550e9dfbb515/',
     // CDN服务器地址
-    REACT_APP_CDN_SERVER: '//cdn.gmsoftdev.com',
+    REACT_APP_CDN_SERVER: '//cdn.gm',
   },
   // plugins: [require('./plugins/plugin-demo')],
 };
