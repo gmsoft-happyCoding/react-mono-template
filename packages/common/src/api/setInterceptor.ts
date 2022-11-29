a3e8cb96b78799924b1580b092d6c0aecaaa69c4
angular-moon <xiaoxuss@163.com>
2022-11-30 01:54:20 +0800

    update auth-sdk@1.5.0

--- packages/common/src/api/setInterceptor.ts
+++ packages/common/src/api/setInterceptor.ts
@@ -1,14 +1,8 @@
-import { useResponseInterceptor } from './util';
-import { showNetworkError, stateContainer } from '../utils';
-import { axiosSSOInterceptor } from '@gmsoft/auth-sdk';
+import { useRequestInterceptor, useResponseInterceptor } from './util';
+import { showNetworkError } from '../utils';
+import { axiosTokenInterceptor } from '@gmsoft/auth-sdk';
 
-useResponseInterceptor(
-  undefined,
-  axiosSSOInterceptor({
-    djcGatewayBaseUrl: process.env.REACT_APP_DJC_GATEWAY_BASE,
-    dispatch: stateContainer._store.dispatch,
-  })
-);
+useRequestInterceptor(axiosTokenInterceptor());
 
 const errorHandler = error => {
   showNetworkError(error);
