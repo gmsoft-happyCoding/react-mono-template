738cc38c7a578868edff90ba9cce995db54a4685
和风柒月 <julysunshine0914@gmail.com>
2023-06-15 15:28:38 +0800

    fix：组件加载位置修改

--- packages/app/src/app/WhatToEat/WhatToEat.tsx
+++ packages/app/src/app/WhatToEat/WhatToEat.tsx
@@ -33,8 +33,11 @@ function WhatToEat() {
         如果你是第一次看到该模板, 建议你先阅读一下项目根目录的 README.md
         了解一下项目结构和有哪些命令可以使用
       </SubTitle>
-      <CloudComponent url="http://localhost:3030/static/js/WhatToEat.js" />
-      <CloudComponent url="http://localhost:3030/static/js/WhatToEat.js" defaultMode="search" />
+      <CloudComponent url="http://localhost:3030/{{projectDeployNameComponents}}/static/js/WhatToEat.js" />
+      <CloudComponent
+        url="http://localhost:3030/{{projectDeployNameComponents}}/static/js/WhatToEat.js"
+        defaultMode="search"
+      />
       <WhatToEatRemote />
       {/* 通过组件名字 从 registry.xcjdev1.gm 加载 */}
       <CloudComponent name="test-project/WhatToEat" defaultMode="search" />
