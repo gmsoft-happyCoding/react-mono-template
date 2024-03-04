5659e775c0c2caea8728b130e16ea0fc91f12d35
angular-moon <xiaoxuss@gmail.com>
2024-03-04 18:08:17 +0800

    improve exportComponents

--- packages/components/config/exportComponents.js
+++ packages/components/config/exportComponents.js
@@ -1,12 +1,20 @@
 const path = require('path');
 const paths = require('./paths');
-
-// 要导出的组件
-const exportComponentNames = ['WhatToEat'];
+const exportComponentConfig = require('./exportComponentsPath.config');
 
 // 规约为 { component: componentPath }
-const exportComponents = exportComponentNames.reduce((components, component) => {
-  components[component] = path.join(paths.appSrc, 'components', component);
+const exportComponents = exportComponentConfig.reduce((components, component) => {
+  if (typeof component === 'string') {
+    /**
+     * component name 与 path 相同
+     */
+    components[component] = path.join(paths.appSrc, 'components', component);
+  } else {
+    /**
+     * component name 与 path 不同，使用 component[0] = key, component[1] = path
+     */
+    components[component[0]] = path.join(paths.appSrc, 'components', component[1]);
+  }
   return components;
 }, {});
 
---
+++ packages/components/config/exportComponentsPath.config.js
@@ -0,0 +1,15 @@
+// 要导出的组件
+const exportComponentConfig = [
+  /**
+   * 单位管理
+   */
+  'WhatToEat',
+  /**
+   * path 不包含 @/src/components 部分
+   * 例如:
+   * ['DemoComponent', 'demo/DemoComponent']
+   */
+  // ['name', 'path'],
+];
+
+module.exports = exportComponentConfig;
