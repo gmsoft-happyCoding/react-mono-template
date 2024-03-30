368e8fc142282c24f3ef35b4c3743caf89e0f2c2
angular-moon <xiaoxuss@gmail.com>
2024-03-30 11:18:26 +0800

    improve interceptorIgnoreError

--- packages/common/src/api/setInterceptor.ts
+++ packages/common/src/api/setInterceptor.ts
@@ -1,20 +1,26 @@
-import { AxiosError, AxiosInstance } from 'axios';
+import axios, { AxiosError, AxiosInstance } from 'axios';
 import { axiosTokenInterceptor } from '@gmsoft/auth-sdk';
 import { showNetworkError } from '../utils';
 import type { Opts } from './Opts';
 
 export default (instance: AxiosInstance) => {
-  instance.interceptors.request.use(axiosTokenInterceptor(
-    {
+  instance.interceptors.request.use(
+    axiosTokenInterceptor({
       platDomain:
         process.env.NODE_ENV === 'development'
           ? process.env['business.dev-plat-domain']
           : location.host,
-    }
-  ));
+    })
+  );
 
   const errorHandler = (error: AxiosError & { config: Opts }) => {
-    if (!error.config.interceptorIgnoreError) showNetworkError(error);
+    /**
+     * 忽略或主动取消请求时不显示错误信息
+     */
+    if (!error?.config?.interceptorIgnoreError && !axios.isAxiosError(error)) {
+      showNetworkError(error);
+    }
+
     return Promise.reject(error);
   };
 
